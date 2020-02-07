659080cd8098523904b0665b50445439f52903f8
rentax <123456>
2020-02-07 18:36:47 +0530

    Save the states When activity is not visible

--- app/src/main/java/com/stopwatch/StopWatchActivity.java
+++ app/src/main/java/com/stopwatch/StopWatchActivity.java
@@ -1,24 +1,54 @@
 package com.stopwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StopWatchActivity extends AppCompatActivity {
 
     private TextView textView;
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("running", running);
+        outState.putBoolean("wasRunning", wasRunning);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
-        textView = (TextView)findViewById(R.id.tvTime);
+        textView = (TextView) findViewById(R.id.tvTime);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
         runTimer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (wasRunning) {
+            running = true;
+        }
     }
 
     public void onClickStart(View view) {
@@ -34,21 +64,21 @@ public class StopWatchActivity extends AppCompatActivity {
         seconds = 0;
     }
 
-    private void runTimer(){
+    private void runTimer() {
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds /3600;
-                int minutes = (seconds%3600)/60;
-                int secs = seconds%60;
-                String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
                 textView.setText(time);
-                if (running){
+                if (running) {
                     seconds++;
                 }
 
-                handler.postDelayed(this,1000);
+                handler.postDelayed(this, 1000);
             }
         });
 
